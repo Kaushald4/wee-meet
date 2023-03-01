249f58f65f26b86faf17306c9ded26f591a12c96
Kaushal Mehta <Kaushalkumar304@gmail.com>
2023-03-01 23:23:18 +0530

    fixing env variable names

--- src/context/socket.jsx
+++ src/context/socket.jsx
@@ -5,7 +5,7 @@ const SocketContext = createContext(null);
 
 const SocketProvider = ({ children }) => {
     const socket = useMemo(() => {
-        return io(process.env.VITE_APP_BACKEND_URL, {
+        return io(process.env.REACT_APP_BACKEND_URL, {
             // return io("http://localhost:4000", {
             withCredentials: true,
         });
--- src/service/weMeetApi.js
+++ src/service/weMeetApi.js
@@ -4,9 +4,9 @@ export const weMeetApi = createApi({
     reducerPath: "wemeetApi",
     baseQuery: fetchBaseQuery({
         baseUrl:
-            process.env.VITE_APP_MYENV === "development"
+            process.env.REACT_APP_MYENV === "development"
                 ? "http://localhost:4000/api/v1"
-                : process.env.VITE_APP_BACKEND_URL,
+                : process.env.REACT_APP_BACKEND_URL,
     }),
     endpoints: () => ({}),
 });
