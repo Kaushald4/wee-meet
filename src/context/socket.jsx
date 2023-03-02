b082db02222cc2f710785ecf40158052c82c07a4
Kaushal Mehta <Kaushalkumar304@gmail.com>
2023-03-02 14:10:09 +0530

    updating server url

--- src/context/socket.jsx
+++ src/context/socket.jsx
@@ -5,7 +5,7 @@ const SocketContext = createContext(null);
 
 const SocketProvider = ({ children }) => {
     const socket = useMemo(() => {
-        return io(process.env.REACT_APP_BACKEND_URL, {
+        return io(process.env.REACT_APP_SOCKET_PATH, {
             // return io("http://localhost:4000", {
             autoConnect: true,
         });
